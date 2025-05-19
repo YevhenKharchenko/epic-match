e04bec4eb69fd75a6ba3b98070868585b550f359
YevhenKharchenko <evgenvalerych@gmail.com>
2025-05-19 18:58:11 +0300

    Add about, features and characters sections

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/header.js';
-import './js/uniqueness.js';
-import './js/reviews.js';
+import './js/features.js';
 import './js/gallery.js';
+import './js/reviews.js';
 import './js/faq.js';
 import './js/banner.js';
